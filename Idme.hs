ae3cd566e92fff99c74459c428ac6135fa564d98
Adam Wagner <awagner83@gmail.com>
2011-08-03 17:25:25 -0400

    Initial commit.

---
+++ Idme.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import Control.Concurrent
+import System.IO
+import Network
+
+type NodeId = String
+type IdSequence = [String]
+
+-- Char dividing incrementing-id from node-id
+idNodeDelim :: Char
+idNodeDelim = '.'
+
+-- Init system and kick-off main loop
+main :: IO b
+main = do
+    txHandle <- openFile "txlog" AppendMode
+    hSetBuffering txHandle NoBuffering
+    socket <- listenOn $ PortNumber 8888
+    idLoop (idSequence 1 "1") txHandle socket
+
+-- Main id-serving loop
+idLoop :: IdSequence -> Handle -> Socket -> IO b
+idLoop (id:ids) txHandle sock = do
+    hPutChar txHandle '.'
+    (clientH, _, _) <- accept sock
+
+    -- Remaining work need not be synchronous
+    forkIO $ do
+        hPutStr clientH id
+        hClose clientH
+
+    idLoop ids txHandle sock
+
+-- Sequence of ids to hand to clients.
+idSequence :: Integer -> NodeId -> IdSequence
+idSequence start nodeId = map format [start..]
+    where format x = (show x) ++ (idNodeDelim : nodeId)
