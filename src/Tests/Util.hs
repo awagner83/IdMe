74fa1d192fc42fa06e20a350c62d2cc6abed3c70
Adam Wagner <awagner83@gmail.com>
2011-09-28 17:54:03 -0400

    pairOf property test.

--- src/Tests/Util.hs
+++ src/Tests/Util.hs
@@ -4,12 +4,14 @@ import Control.Monad.Writer
 import Test.Framework (testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
-import Idme.Util (constF, (>>-))
+import Idme.Util (constF, (>>-), pairOf)
 
 -- | Tests provided by this test-module
 tests = testGroup "Utility"
     [ testProperty "constF" prop_constF
-    , testProperty ">--"    prop_util_operator1 ]
+    , testProperty ">--"    prop_util_operator1
+    , testProperty "pairOf" prop_pairOf
+    ]
 
 -- | Test constF with Writer [Bool]
 prop_constF :: Bool -> Bool
@@ -19,4 +21,7 @@ prop_constF x = let test = runWriter $ constF (tell . return) x
 -- | Test >>- with Writer [Bool]
 prop_util_operator1 :: Bool -> Bool
 prop_util_operator1 x = runWriter (return x >>- tell . return) == (x, [x])
+
+prop_pairOf :: Bool -> Bool
+prop_pairOf a = let p = pairOf a in fst p == a && snd p == a
 
